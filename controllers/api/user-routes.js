4fa550d3e7f987fc4d428692453bb29722145df9
janimuhlestein <janimuhlestein@gmail.com>
2024-07-03 23:18:21 -0600

    added password check and login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -44,9 +44,35 @@ router.post('/', (req, res) => {
     });
 });
 
+//login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user with that email address!' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in.' });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
 //PUT api/users/#id
 router.put('/:id', (req, res) => {
-    User.update({
+    User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
