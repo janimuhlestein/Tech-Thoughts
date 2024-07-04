4dae4f93589eea389eb54e9e1dbda960c43d9d8c
janimuhlestein <janimuhlestein@gmail.com>
2024-07-03 23:55:35 -0600

    added post model

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { underscoredIf } = require('sequelize/lib/utils');
+
+class Post extends Model {
+
+}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                isURL: true
+            }
+        },
+        post_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post
