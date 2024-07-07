6696f56943791813168f16392de2ab8f7603f4bf
janimuhlestein <janimuhlestein@gmail.com>
2024-07-06 22:04:44 -0600

    comment

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -25,8 +25,24 @@ router.get('/', (req, res) => {
 
 //get one post
 router.get('/:id', (req, res) => {
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+    Post.findOne({
+        attributes: ['id', 'post_url', 'post_text', 'title', 'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post_id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+        res.json(dbPostData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
